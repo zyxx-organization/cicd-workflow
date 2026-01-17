fcf4c13a84710df69e025bacf606b89fce4f2451
zyxx66 <914731317@qq.com>
2026-01-17 20:58:53 +0900

    update deploy script to sync with origin/master and restart service

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -8,8 +8,12 @@ echo "🚀 Deploying $PROJECT_NAME"
 
 cd "$BASE_DIR/$PROJECT_NAME"
 
-git pull --ff-only
+echo "🔄 Sync code from origin/master"
+git fetch origin
+git checkout master
+git reset --hard origin/master
 
+echo "🔁 Restart service"
 sudo systemctl restart "$PROJECT_NAME"
 
 echo "✅ $PROJECT_NAME deployed"
